0974fae2e90a0367263a909ebae1b75f5d8374e0
Oliver Strickson <ostrickson@turing.ac.uk>
2018-01-23 17:32:21 +0000

    Move implementation of ContextClear::eval to default implementation of Context::eval

--- sketch/context-clear.hpp
+++ sketch/context-clear.hpp
@@ -53,64 +53,6 @@ public:
 		throw std::runtime_error("Unknown op");
 	}
 
-	// each Context concrete class provides its own eval method,
-	// and also a compile method which can perform any
-	// library-specific optimization.  Here we only provide 'eval'
-	// and rely on the default implementation of compile in the
-	// base Context class.
-	std::chrono::duration<double, std::micro>
-	eval(const Circuit& circ,
-	     const std::list<Ciphertext>& input_vals,
-	     std::list<Ciphertext>& output_vals) {
-		std::unordered_map<std::string, Ciphertext> eval_map;
-
-		// add Circuit::inputs and inputs into the map
-		auto input_vals_it = input_vals.begin();
-		auto input_wires_it = circ.get_inputs().begin();
-		const auto input_wires_end = circ.get_inputs().end();
-		for (; input_vals_it != input_vals.end() || input_wires_it != input_wires_end;
-		     ++input_vals_it, ++input_wires_it)
-		{
-			eval_map.insert({input_wires_it->get_name(), *input_vals_it});
-		}
-
-		// error check: both iterators should be at the end
-		if (input_vals_it != input_vals.end() || input_wires_it != input_wires_end)
-			throw std::runtime_error("Number of inputs doesn't match");
-
-
-		// This is where the actual evaluation occurs.  For
-		// each assignment, look up the input Wires in the
-		// map, insert the output wire (of the gate) with the
-		// required name into eval_map.
-
-		typedef std::chrono::duration<double, std::micro> microsecond;
-		typedef std::chrono::high_resolution_clock high_res_clock;
-		auto start_time = high_res_clock::now();
-		
-		for (const Assignment assn : circ.get_assignments()) {
-			// throws out_of_range if not present in the map
-			Ciphertext input1 = eval_map.at(assn.get_input1().get_name());
-			Ciphertext input2 = eval_map.at(assn.get_input2().get_name());
-			auto op = get_op(assn.get_op());
-			Ciphertext output = op(input1, input2);
-			eval_map.insert({assn.get_output().get_name(), output});
-		}
-		
-		auto end_time = high_res_clock::now();
-		microsecond duration = microsecond(end_time - start_time);
-
-		
-		// Look up the required outputs in the eval_map and
-		// push them onto output_vals.
-		auto output_wires_it = circ.get_outputs().begin();
-		auto output_wires_end = circ.get_outputs().end();
-		for (; output_wires_it != output_wires_end; ++output_wires_it) {
-			output_vals.push_back(eval_map.at(output_wires_it->wire.get_name()));
-		}
-				
-		return duration;
-	}
 };
 
 #endif // CONTEXT_CLEAR_HPP
