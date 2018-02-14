70fb7302df765f08f2ff9b901b179dc432579ab7
Nick Barlow <nbarlow@turing.ac.uk>
2018-02-14 17:07:59 +0000

    add tests directory containing simple unit tests for clear context

---
+++ sketch/tests/test-clear-multiply.cpp~
@@ -0,0 +1,45 @@
+#include <memory>
+
+#include "context-clear.hpp"
+
+#include "circuit-repo.hpp"
+
+int main(void) {
+
+  
+    //// instantiate the Circuit Repository
+  CircuitRepo cr;
+
+  //// build a circuit with a specified depth of a specified gate
+  
+  Circuit C = cr.create_circuit(Gate::Add, 3);
+  std::cout << C;
+  
+  
+  ContextClear ctx;
+  
+  ContextClear::CircuitEvaluator run_circuit;
+  run_circuit = ctx.compile(C);
+	
+  std::list<ContextClear::Plaintext> plaintext_inputs = {6, 9, 25, 67};
+  std::list<ContextClear::Ciphertext> ciphertext_inputs;
+  
+  for (ContextClear::Plaintext pt: plaintext_inputs)
+    ciphertext_inputs.push_back(ctx.encrypt(pt));
+  
+  std::list<ContextClear::Ciphertext> ciphertext_outputs;
+  using microsecond = std::chrono::duration<double, std::micro>;
+  microsecond time = run_circuit(ciphertext_inputs, ciphertext_outputs);
+  
+  std::list<ContextClear::Plaintext> plaintext_outputs;
+  for (ContextClear::Ciphertext ct: ciphertext_outputs) {
+    ContextClear::Plaintext pt = ctx.decrypt(ct);
+    plaintext_outputs.push_back(pt);
+    std::cout << "output: "<<std::to_string(pt) << std::endl;
+  }
+  std::cout << "time was " << time.count() << " microseconds\n";
+
+  if ( plaintext_outputs.front() == 107) return 0;
+  return -1;
+  
+}
---
+++ sketch/tests/test-clear-subtract.cpp
@@ -0,0 +1,48 @@
+#include <memory>
+
+#include "context-clear.hpp"
+
+#include "circuit-repo.hpp"
+
+int main(void) {
+
+  
+    //// instantiate the Circuit Repository
+  CircuitRepo cr;
+
+  //// build a circuit with a specified depth of a specified gate
+  
+  Circuit C = cr.create_circuit(Gate::Subtract, 3);
+  std::cout << C;
+  
+  
+  ContextClear ctx;
+  
+  ContextClear::CircuitEvaluator run_circuit;
+  run_circuit = ctx.compile(C);
+	
+  std::list<ContextClear::Plaintext> plaintext_inputs = {144, 35, 6, 7};
+  std::list<ContextClear::Ciphertext> ciphertext_inputs;
+
+
+
+  
+  for (ContextClear::Plaintext pt: plaintext_inputs) {
+    ciphertext_inputs.push_back(ctx.encrypt(pt));
+  }
+  std::list<ContextClear::Ciphertext> ciphertext_outputs;
+  using microsecond = std::chrono::duration<double, std::micro>;
+  microsecond time = run_circuit(ciphertext_inputs, ciphertext_outputs);
+  
+  std::list<ContextClear::Plaintext> plaintext_outputs;
+  for (ContextClear::Ciphertext ct: ciphertext_outputs) {
+    ContextClear::Plaintext pt = ctx.decrypt(ct);
+    plaintext_outputs.push_back(pt);
+    std::cout << "output: "<<std::to_string(pt) << std::endl;
+  }
+  std::cout << "time was " << time.count() << " microseconds\n";
+
+  if ( plaintext_outputs.front() == 96 ) return 0;
+  return -1;
+  
+}
