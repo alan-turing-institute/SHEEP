e40b7967d91c9dee2deeb5b2ebd82cb9ae82a1d9
Oliver Strickson <ostrickson@turing.ac.uk>
2018-02-16 09:54:52 +0000

    Rewrite to replace some functionality not supported by gcc 4.8

--- sketch/tests/circuit-test-util.hpp
+++ sketch/tests/circuit-test-util.hpp
@@ -6,8 +6,20 @@ bool eval_encrypted_check_equal(ContextT ctx, Circuit circ,
 				PTcontainer in, PTcontainer out_expected)
 {
 	PTcontainer out(eval_with_encrypted(ctx, circ, in));
-	return std::equal(out.begin(), out.end(),
-			  out_expected.begin(), out_expected.end());
+
+//      Should be equivalent to the C++17 statement:
+//	return std::equal(out.begin(), out.end(),
+//			  out_expected.begin(), out_expected.end());
+
+	auto outIt = out.begin();
+	auto expectedIt = out_expected.begin();
+	for ( ; outIt != out.end() || expectedIt != out_expected.end();
+	      ++outIt, ++expectedIt)
+	{
+		if (*outIt != *expectedIt) return false;
+	}
+
+	return (outIt == out.end() && expectedIt == out_expected.end());
 }
 
 #endif // CIRCUIT_TEST_UTIL_HPP
