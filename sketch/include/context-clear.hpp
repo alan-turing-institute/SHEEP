366333ed051b645de5cb5424b9e4cdb0c48639c0
Oliver Strickson <ostrickson@turing.ac.uk>
2018-02-19 09:54:53 +0000

    Ripple carry adder for ContextClear (modified to use a signed type)

--- sketch/include/context-clear.hpp
+++ sketch/include/context-clear.hpp
@@ -3,9 +3,9 @@
 
 #include "circuit.hpp"
 #include "context.hpp"
+#include "bits.hpp"
 
-
-class ContextClear : public Context<uint8_t, uint8_t> {
+class ContextClear : public Context<int8_t, int8_t> {
 public:
 	Ciphertext encrypt(Plaintext p) {
 	  std::cout<<"encrypting plaintext "<<std::to_string(p)<<std::endl;
@@ -15,7 +15,36 @@ public:
 	Plaintext decrypt(Ciphertext c) {
 		return c; // plaintext and ciphertext are the same for this context
 	}
-	
+
+	std::pair<bool,bool> HalfAdder(bool a, bool b) {
+		bool sum, carry;
+		sum = (a != b);
+		carry = (a && b);
+		return std::make_pair(sum, carry);
+	}
+
+	std::pair<bool,bool> FullAdder(bool a, bool b, bool carry_in) {
+		bool sum, carry_out, s1, c1, c2;
+		std::tie(s1, c1) = HalfAdder(a, b);
+		std::tie(sum, c2) = HalfAdder(s1, carry_in);
+		carry_out = c1 || c2;
+		return std::make_pair(sum, carry_out);		
+	}
+
+	Ciphertext RippleCarryAdd(Ciphertext a, Ciphertext b) {
+		Ciphertext result;
+		bool sum, carry;
+		std::tie(sum, carry) = HalfAdder(bit(0,a), bit(0,b));
+		set_bit(0, result, sum);
+		// Note that the loop starts at ONE, since we have
+		// already computed the zeroth bit above
+		for (size_t i = 1; i < BITWIDTH(Plaintext); ++i) {
+			std::tie(sum, carry) = FullAdder(bit(i,a), bit(i,b), carry);
+			set_bit(i, result, sum);
+		}
+		return result;
+	}
+
 	Ciphertext Add(Ciphertext a, Ciphertext b) {
 	  std::cout<<"Using clear context's ADD"<<std::endl;
 	  return (a + b);
