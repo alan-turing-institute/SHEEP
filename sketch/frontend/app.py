fcf940eb41bbad1191185506a212a6b718cce7ca
Nick Barlow <nbarlow@turing.ac.uk>
2018-02-23 16:43:56 +0000

    add frontend

--- sketch/frontend/app.py
+++ sketch/frontend/app.py
@@ -4,6 +4,7 @@ Flask app for SHEEP.
 
 from flask import Flask, render_template
 
+import subprocess
 
 app = Flask(__name__)
 
@@ -11,6 +12,21 @@ app = Flask(__name__)
 def homepage():
     return render_template("index.html")
 
+
+@app.route("/new_test")
+def new_test():
+    contexts = ["Clear","HElib","TFHE"]
+    types = ["bool","int8_t","uint8_t"]
+    return render_template("new_test.html",contexts=contexts,types=types)
+
+@app.route("/view_results")
+def results_table():
+    return render_template("results_table.html")
+
+
+@app.route("/execute_test")
+def execute_test():
+    return
 
 
 if __name__ == "__main__":
