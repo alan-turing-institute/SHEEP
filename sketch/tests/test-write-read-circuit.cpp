de1ef7d52ac456bb93a912279bfff9c5c96f889a
Oliver Strickson <ostrickson@turing.ac.uk>
2018-03-05 09:48:21 +0000

    Use Circuit operator== for test-write-read-circuit.cpp

--- sketch/tests/test-write-read-circuit.cpp
+++ sketch/tests/test-write-read-circuit.cpp
@@ -6,43 +6,6 @@
 
 #include "circuit-repo.hpp"
 
-bool check_circuits_equal(const Circuit& c1, const Circuit& c2) {
-
-  bool inputs_same_length= (c1.get_inputs().size() == c2.get_inputs().size());
-  if (! inputs_same_length) return false;
-
-  bool outputs_same_length= (c1.get_outputs().size() == c2.get_outputs().size());
-  if (! outputs_same_length) return false;
-
-  bool assignments_same_length= (c1.get_assignments().size() == c2.get_assignments().size());
-  if (! assignments_same_length) return false;  
-
-  /// check individual inputs and outputs
-  
-  Circuit::WireList c1_inputs = c1.get_inputs();
-  Circuit::WireList c2_inputs = c2.get_inputs();
-  auto c1_it = c1_inputs.begin();
-  auto c2_it = c2_inputs.begin();  
-  while( c1_it != c1_inputs.end() && c2_it != c2_inputs.end() ) {
-    if (c1_it->get_name() != c2_it->get_name()) return false;
-    c1_it++;
-    c2_it++;
-  }
-
-  Circuit::WireList c1_outputs = c1.get_outputs();
-  Circuit::WireList c2_outputs = c2.get_outputs();
-  auto c1out_it = c1_outputs.begin();
-  auto c2out_it = c2_outputs.begin();
-  while( c1out_it != c1_outputs.end() && c2out_it != c2_outputs.end() ) {
-    if (c1out_it->get_name() != c2out_it->get_name()) return false;
-    c1out_it++;
-    c2out_it++;
-  }
-  
-  return true;
-}
-
-
 int main(void) {
 
   std::string filename = "test_circuit.circ";
@@ -64,8 +27,6 @@ int main(void) {
   Circuit C_new;
   std::ifstream infile(filename);
   infile >> C_new;
-  
-  assert(check_circuits_equal(C_orig,C_new));
 
- 
+  assert(C_orig == C_new);
 }
