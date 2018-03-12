4ca0a382ef76ec421c1c31a86473358a1c9dbe23
nbarlowATI <nbarlow@turing.ac.uk>
2018-03-12 09:17:16 +0000

    add select for helib

---
+++ sketch/example/bitonic-sort-helib.cpp
@@ -0,0 +1,35 @@
+#include <cstdint>
+#include <iostream>
+#include <fstream>
+#include "simple-circuits.hpp"
+//#include "context-clear.hpp"
+#include "context-helib.hpp"
+#include "all_equal.hpp"
+
+int main(void)
+{
+	using namespace Sheep::HElib;
+	typedef std::vector<ContextHElib<int8_t>::Plaintext> PtVec;
+
+	std::cout << "Constructing context...\n";
+	ContextHElib<int8_t> ctx;
+
+	std::cout << "The bitonic sorting network is:\n";
+	Circuit bitonic = bitonic_sort(4, false);
+	std::cout << bitonic << std::endl;
+	std::ofstream outputfile("bitonic_sort.sheep");
+	outputfile << bitonic;
+	outputfile.close();
+	
+	PtVec inputs {7, 3, 10, 0};
+
+	std::cout << "Inputs are: ";
+	for (auto x : inputs) std::cout << std::to_string(x) << " ";
+	std::cout << std::endl;
+
+	PtVec sorted = eval_with_plaintexts(ctx, bitonic, inputs);
+
+	std::cout << "Sorted result is: ";
+	for (auto x : sorted) std::cout << std::to_string(x) << " ";
+	std::cout << std::endl;
+}
