c96a74d8ffb125b54ea21e58c1c099d0b324a9f5
Nick Barlow <nbarlow@turing.ac.uk>
2018-03-15 12:11:08 +0000

    consolidate package - move everything from sketch into base dir

---
+++ example/bitonic-sort-helib.cpp
@@ -0,0 +1,34 @@
+#include <cstdint>
+#include <iostream>
+#include <fstream>
+#include "simple-circuits.hpp"
+#include "context-helib.hpp"
+#include "all_equal.hpp"
+
+int main(void)
+{
+	using namespace Sheep::HElib;
+	typedef std::vector<ContextHElib_F2<int8_t>::Plaintext> PtVec;
+
+	std::cout << "Constructing context...\n";
+	ContextHElib_F2<int8_t> ctx;   // paramset, bootstrappable
+
+	std::cout << "The bitonic sorting network is:\n";
+	Circuit bitonic = bitonic_sort(4, false);
+	std::cout << bitonic << std::endl;
+	std::ofstream outputfile("bitonic_sort.sheep");
+	outputfile << bitonic;
+	outputfile.close();
+	
+	PtVec inputs {7, 3, 22, 2 };
+
+	std::cout << "Inputs are: ";
+	for (auto x : inputs) std::cout << std::to_string(x) << " ";
+	std::cout << std::endl;
+
+	PtVec sorted = ctx.eval_with_plaintexts(bitonic, inputs);
+
+	std::cout << "Sorted result is: ";
+	for (auto x : sorted) std::cout << std::to_string(x) << " ";
+	std::cout << std::endl;
+}
