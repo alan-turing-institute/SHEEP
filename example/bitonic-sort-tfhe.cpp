0048e161fc218d2df124465dd780f6be9fc2ae8e
Nick Barlow <nbarlow@turing.ac.uk>
2018-03-20 16:08:07 +0000

    rename bitonic-sorting-circuit to bitonic-sort-tfhe

--- example/bitonic-sort-tfhe.cpp
+++ example/bitonic-sort-tfhe.cpp
@@ -1,26 +1,23 @@
 #include <cstdint>
 #include <iostream>
-#include <fstream>
 #include "simple-circuits.hpp"
-#include "context-helib.hpp"
+//#include "context-clear.hpp"
+#include "context-tfhe.hpp"
 #include "all_equal.hpp"
 
 int main(void)
 {
 	using namespace SHEEP;
-	typedef std::vector<ContextHElib_F2<int8_t>::Plaintext> PtVec;
+	typedef std::vector<ContextTFHE<int8_t>::Plaintext> PtVec;
 
 	std::cout << "Constructing context...\n";
-	ContextHElib_F2<int8_t> ctx;   // paramset, bootstrappable
+	ContextTFHE<int8_t> ctx;
 
 	std::cout << "The bitonic sorting network is:\n";
 	Circuit bitonic = bitonic_sort(4, false);
 	std::cout << bitonic << std::endl;
-	std::ofstream outputfile("bitonic_sort.sheep");
-	outputfile << bitonic;
-	outputfile.close();
-	
-	PtVec inputs {7, 3, 22, 2 };
+
+	PtVec inputs {7, 3, 10, 0};
 
 	std::cout << "Inputs are: ";
 	for (auto x : inputs) std::cout << std::to_string(x) << " ";
