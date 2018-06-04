bd8670dd0eef297490ee4f775640595883f29389
Nick Barlow <nbarlow@turing.ac.uk>
2018-06-04 15:21:13 +0100

    separate out source code and main for sheep-server

---
+++ example/run-sheep-server.cpp
@@ -0,0 +1,65 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <sys/time.h>
+
+#include "cpprest/json.h"
+#include "cpprest/http_listener.h"
+#include "cpprest/uri.h"
+#include "cpprest/asyncrt_utils.h"
+#include "sheep-server.hpp"
+
+using namespace std;
+using namespace web; 
+using namespace utility;
+using namespace http;
+using namespace web::http::experimental::listener;
+
+std::unique_ptr<SheepServer> g_http;
+
+void on_initialize(const string_t& address)
+{
+    // Build our listener's URI from the configured address and the hard-coded path "SheepServer/Action"
+
+    uri_builder uri(address);
+    uri.append_path(U("SheepServer/Action/"));
+
+    auto addr = uri.to_uri().to_string();
+	g_http = std::unique_ptr<SheepServer>(new SheepServer(addr));
+	g_http->open().wait();
+    
+    ucout << utility::string_t(U("Listening for requests at: ")) << addr << std::endl;
+
+    return;
+}
+
+void on_shutdown()
+{
+	g_http->close().wait();
+    return;
+}
+
+int main(int argc, const char** argv)
+{
+    utility::string_t port = U("34568");
+    if(argc == 2)
+    {
+        port = argv[1];
+    }
+
+    utility::string_t address = U("http://localhost:");
+    address.append(port);
+
+    on_initialize(address);
+    std::cout << "Press ENTER to exit." << std::endl;
+
+    std::string line;
+    std::getline(std::cin, line);
+
+    on_shutdown();
+    return 0;
+}
