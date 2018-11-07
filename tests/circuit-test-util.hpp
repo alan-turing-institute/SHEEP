c752ab0f3b6f8a8a707de2e05f0451f5f3767fd1
Nick Barlow <nbarlow@turing.ac.uk>
2018-11-07 16:16:20 +0000

    apply Google style to all C++

--- tests/circuit-test-util.hpp
+++ tests/circuit-test-util.hpp
@@ -4,11 +4,10 @@
 #include "all_equal.hpp"
 
 template <typename ContextT, typename PTcontainer>
-bool eval_encrypted_check_equal(ContextT ctx, Circuit circ,
-				PTcontainer in, PTcontainer out_expected)
-{
-	PTcontainer out(ctx.eval_with_plaintexts(circ, in));
-	return all_equal(out, out_expected);
+bool eval_encrypted_check_equal(ContextT ctx, Circuit circ, PTcontainer in,
+                                PTcontainer out_expected) {
+  PTcontainer out(ctx.eval_with_plaintexts(circ, in));
+  return all_equal(out, out_expected);
 }
 
-#endif // CIRCUIT_TEST_UTIL_HPP
+#endif  // CIRCUIT_TEST_UTIL_HPP
