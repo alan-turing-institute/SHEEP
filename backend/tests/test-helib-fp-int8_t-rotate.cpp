dd9afd38345332bb36bf190017f61c0cf332147e
Nick Barlow <nbarlow@turing.ac.uk>
2018-12-17 16:57:11 +0000

    test rotate with differently packed slots

--- backend/tests/test-helib-fp-int8_t-rotate.cpp
+++ backend/tests/test-helib-fp-int8_t-rotate.cpp
@@ -23,22 +23,23 @@ int main(void) {
 
   // The type of the wires in circ are unsigned bools
   ContextHElib_Fp<int8_t> ctx;
-
+  ctx.set_parameter("BaseParamSet",0);
+  ctx.configure();
   // inputs is vector of vectors
-  std::vector<std::vector<int8_t>> inputs = {{1, 2, 3, 4}};
+  std::vector<std::vector<int8_t>> inputs = {{1, 2, 3}};
   // const_inputs is vector (same across slots)
-  std::vector<long> const_inputs = {2};
-  std::vector<int8_t> exp_values = {3, 4, 1, 2};
+  std::vector<long> const_inputs = {-2};
+  std::vector<int8_t> exp_values = {2, 1, 3};
 
   std::vector<std::vector<int8_t>> result =
       ctx.eval_with_plaintexts(C, inputs, const_inputs);
 
   std::cout << "Original vector: ";
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 3; i++) {
     std::cout << std::to_string(inputs[0][i]) << " ";
   }
   std::cout << std::endl << "Rotated vector:  ";
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 3; i++) {
     std::cout << std::to_string(result[0][i]) << " ";
   }
 
