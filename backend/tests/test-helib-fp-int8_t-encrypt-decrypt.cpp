5eb1b8ed20e9e5ddd99871cc36cf73720aea6b65
Nick Barlow <nbarlow@turing.ac.uk>
2018-12-17 16:56:18 +0000

    add slots to test

--- backend/tests/test-helib-fp-int8_t-encrypt-decrypt.cpp
+++ backend/tests/test-helib-fp-int8_t-encrypt-decrypt.cpp
@@ -12,12 +12,14 @@ typedef ContextHElib_Fp<int8_t>::Ciphertext Ciphertext;
 // Encrypt a value, decrypt the result, and check that we are
 // left with the original value.
 void test_single(ContextHElib_Fp<int8_t>& context) {
-  std::vector<Plaintext> pt_orig = {123};
+  std::vector<Plaintext> pt_orig = {123,45};
   std::vector<Plaintext> pt_new = context.decrypt(context.encrypt(pt_orig));
 
-  std::cout << std::to_string(pt_orig[0]) << " = " << std::to_string(pt_new[0])
-            << std::endl;
-  assert(pt_orig[0] == pt_new[0]);
+  for (int i=0; i< pt_orig.size(); i++) {
+    std::cout << std::to_string(pt_orig[i]) << " = " << std::to_string(pt_new[i])
+	      << std::endl;
+    assert(pt_orig[i] == pt_new[i]);
+  }
 }
 
 int main(void) {
