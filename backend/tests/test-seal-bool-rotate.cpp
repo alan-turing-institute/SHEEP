2909d02683a2b91feb90036b7c9f41bf338e3b5c
nbarlowATI <nbarlow@turing.ac.uk>
2018-12-18 16:09:05 +0000

    improve rotate for SEAL

--- backend/tests/test-seal-bool-rotate.cpp
+++ backend/tests/test-seal-bool-rotate.cpp
@@ -26,24 +26,45 @@ int main(void) {
   ctx.set_parameter("BaseParamSet",0);
   ctx.configure();
   // inputs is vector of vectors
-  std::vector<std::vector<bool>> inputs = {{1, 0, 0, 0, 1, 1}};
+  std::vector<std::vector<bool>> inputs = {{1, 0, 0 , 0, 0}};
   // const_inputs is vector (same across slots)
-  std::vector<long> const_inputs = {2};
-  std::vector<bool> exp_values = {1, 1, 1, 0, 0, 0};
+  std::vector<long> const_inputs = {-2};
+  std::vector<bool> exp_values = {0, 0, 0, 1, 0};
 
   std::vector<std::vector<bool>> result =
       ctx.eval_with_plaintexts(C, inputs, const_inputs);
 
   std::cout << "Original vector: ";
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < exp_values.size(); i++) {
     std::cout << std::to_string(inputs[0][i]) << " ";
   }
   std::cout << std::endl << "Rotated vector:  ";
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < exp_values.size(); i++) {
     std::cout << std::to_string(result[0][i]) << " ";
   }
+  for (int i=0; i < exp_values.size(); i++) {
+    assert(exp_values[i] == result[0][i]);
+  }
+  /// now rotate right
+  inputs = {{0,0, 0, 1, 0}};
+  // const_inputs is vector (same across slots)
+  const_inputs = {2};
+  exp_values = {1,0,0,0,0};
 
-  assert(result.front() == exp_values);
+  result =
+      ctx.eval_with_plaintexts(C, inputs, const_inputs);
+
+  std::cout << "Original vector: ";
+  for (int i = 0; i < exp_values.size(); i++) {
+    std::cout << std::to_string(inputs[0][i]) << " ";
+  }
+  std::cout << std::endl << "Rotated vector:  ";
+  for (int i = 0; i < exp_values.size(); i++) {
+    std::cout << std::to_string(result[0][i]) << " ";
+  }
+  for (int i=0; i < exp_values.size(); i++) {
+    assert(exp_values[i] == result[0][i]);
+  }
 
   std::cout << std::endl;
 }
--- backend/tests/test-seal-uint8_t-rotate.cpp
+++ backend/tests/test-seal-uint8_t-rotate.cpp
@@ -26,24 +26,45 @@ int main(void) {
   ctx.set_parameter("BaseParamSet",0);
   ctx.configure();
   // inputs is vector of vectors
-  std::vector<std::vector<uint8_t>> inputs = {{1, 2, 3, 4}};
+  std::vector<std::vector<uint8_t>> inputs = {{1, 2, 3, 4, 5, 6}};
   // const_inputs is vector (same across slots)
-  std::vector<long> const_inputs = {2};
-  std::vector<uint8_t> exp_values = {3, 4, 1, 2};
+  std::vector<long> const_inputs = {-2};
+  std::vector<uint8_t> exp_values = {3, 4, 5, 6, 1, 2};
 
   std::vector<std::vector<uint8_t>> result =
       ctx.eval_with_plaintexts(C, inputs, const_inputs);
 
   std::cout << "Original vector: ";
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < exp_values.size(); i++) {
     std::cout << std::to_string(inputs[0][i]) << " ";
   }
   std::cout << std::endl << "Rotated vector:  ";
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < exp_values.size(); i++) {
     std::cout << std::to_string(result[0][i]) << " ";
   }
+  for (int i=0; i < exp_values.size(); i++) {
+    assert(exp_values[i] == result[0][i]);
+  }
+  /// now rotate right
+  inputs = {{1, 2, 3, 4, 5, 6}};
+  // const_inputs is vector (same across slots)
+  const_inputs = {2};
+  exp_values = {5, 6, 1, 2, 3, 4};
 
-  assert(result.front() == exp_values);
+  result =
+      ctx.eval_with_plaintexts(C, inputs, const_inputs);
+  std::cout<<std::endl;
+  std::cout << "Original vector: ";
+  for (int i = 0; i < exp_values.size(); i++) {
+    std::cout << std::to_string(inputs[0][i]) << " ";
+  }
+  std::cout << std::endl << "Rotated vector:  ";
+  for (int i = 0; i < exp_values.size(); i++) {
+    std::cout << std::to_string(result[0][i]) << " ";
+  }
+  for (int i=0; i < exp_values.size(); i++) {
+    assert(exp_values[i] == result[0][i]);
+  }
 
   std::cout << std::endl;
 }
